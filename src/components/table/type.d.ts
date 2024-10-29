fc4c37439dd8710a2d001c40bf921523513aa338
[Fateme-Sharif-Dini] <[fgsharifdini@gmail.com]>
2024-10-29 12:39:06 +0330

    fix: move Data types to type.d.ts file

--- src/components/table/type.d.ts
+++ src/components/table/type.d.ts
@@ -1,12 +1,35 @@
 export type ColumnProps<T> = {
-    key: string;
-    title: string | ReactElement;
-    render?: (column: ColumnProps<T>, item: T) => ReactElement;
-  }
-  
-  type Props<T> = {
-    columns: Array<ColumnProps<T>>;
-    data?: T[];
-    className?: string;
-  }
-  
+  key: string;
+  title: string | ReactElement;
+  render?: (column: ColumnProps<T>, item: T) => ReactElement;
+};
+
+type Props<T> = {
+  columns: Array<ColumnProps<T>>;
+  data?: T[];
+  className?: string;
+};
+
+// Data type definition
+export type Data = {
+  id: number;
+  productCode: string;
+  SKU: string;
+  location: string;
+  origin: string;
+  year: string;
+  Price: string;
+  label: "has label" | "doesn't have" | "incorrect" | "unclear";
+  box:
+    | "has box"
+    | "has/own/intact"
+    | "has/own/damaged"
+    | "has/other/intact"
+    | "has/other/damaged";
+  status:
+    | "new"
+    | "new(damaged)"
+    | "used(almost new)"
+    | "used(brocken)"
+    | "not used(brocken)";
+};
