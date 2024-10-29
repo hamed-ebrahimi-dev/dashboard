d06aeebbe2857504710c0ee72af65798e4a8de52
[Fateme-Sharif-Dini] <[fgsharifdini@gmail.com]>
2024-10-29 12:39:06 +0330

    fix: move Data types to type.d.ts file

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,30 +1,6 @@
 import { data } from "@/MOCDATA";
 import Table from "@/src/components/table/Table";
-import { ColumnProps } from "@/src/components/table/type";
-
-// Data type definition
-export type Data = {
-  id: number;
-  productCode: string;
-  SKU: string;
-  location: string;
-  origin: string;
-  year: string;
-  Price: string;
-  label: "has label" | "doesn't have" | "incorrect" | "unclear";
-  box:
-    | "has box"
-    | "has/own/intact"
-    | "has/own/damaged"
-    | "has/other/intact"
-    | "has/other/damaged";
-  status:
-    | "new"
-    | "new(damaged)"
-    | "used(almost new)"
-    | "used(brocken)"
-    | "not used(brocken)";
-};
+import { ColumnProps, Data } from "@/src/components/table/type";
 
 const columns: Array<ColumnProps<Data>> = Object.keys(data[0]).map((key) => ({
   title: key.charAt(0).toUpperCase() + key.slice(1),
