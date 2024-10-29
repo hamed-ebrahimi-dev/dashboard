8567b429d1fac8d3aae229b86a17a7dc4df13700
[Fateme-Sharif-Dini] <[fgsharifdini@gmail.com]>
2024-10-29 11:30:05 +0330

    fix: resolve revision

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,3 +1,4 @@
+import { data } from "@/MOCDATA";
 import Table, { ColumnProps } from "@/src/components/table/Table";
 
 // Data type definition
@@ -24,130 +25,6 @@ export type Data = {
     | "not used(brocken)";
 };
 
-// Moc data
-export const data: Data[] = [
-  {
-    id: 1,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 2,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 3,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 4,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 5,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 6,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 7,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 8,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 9,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-  {
-    id: 10,
-    productCode: "165486465",
-    SKU: "64684654644",
-    location: "Tehran - 1B8",
-    origin: "USA",
-    year: "2018",
-    Price: "4,789,456,000",
-    label: "has label",
-    box: "has box",
-    status: "new",
-  },
-];
-
 const columns: Array<ColumnProps<Data>> = Object.keys(data[0]).map((key) => ({
   title: key.charAt(0).toUpperCase() + key.slice(1),
   dataIndex: key as keyof Data,
@@ -157,7 +34,7 @@ const columns: Array<ColumnProps<Data>> = Object.keys(data[0]).map((key) => ({
 const statusPage = () => {
   return (
     <div>
-      <Table data={data} columns={columns}/>
+      <Table data={data} columns={columns} />
     </div>
   );
 };
